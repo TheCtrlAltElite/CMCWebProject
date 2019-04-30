579997658404beda558ea4e6faa06b50b6f14e08
OMATIAS001 <omatias001@vplincsci055.ad.csbsju.edu>
2019-04-29 21:56:47 -0500

    osbaldo admin functionalities done ?

--- src/AdminFunctionalities/AdminFunctionalityController.java
+++ src/AdminFunctionalities/AdminFunctionalityController.java
@@ -89,6 +89,33 @@ public class AdminFunctionalityController {
 					percentAdmitted, percentEnrolled, academicScale, socialScale, qualityScale, emphases);
 		}
 	}
+	
+	
+	/**
+	 * Adds a new university to the database.
+	 * by calling addUniversityInfo() in UniversityController 
+	 */
+	public int editUniversity(String schoolName, String schoolState, String schoolLocation, String schoolControl, int numberStudents,
+			int percentFemale, int verbalSAT, int mathSAT, int schoolExpenses, int percentFinancialAid, int numApplicants,
+			int percentAdmitted, int percentEnrolled, int academicScale, int socialScale, int qualityScale, List<String> emphases) {
+		
+//		return uniController.addUniversityInfo(schoolName, schoolState, schoolLocation, schoolControl, numberStudents,
+//				percentFemale, verbalSAT, mathSAT, schoolExpenses, percentFinancialAid, numApplicants,
+//				percentAdmitted, percentEnrolled, academicScale, socialScale, qualityScale, emphases);
+		
+		
+		if(emphases== null){
+			return uniController.editUniversity(schoolName, schoolState, schoolLocation, schoolControl, numberStudents,
+					percentFemale, verbalSAT, mathSAT, schoolExpenses, percentFinancialAid, numApplicants,
+					percentAdmitted, percentEnrolled, academicScale, socialScale, qualityScale, null);
+		}
+		
+		else {
+			return uniController.editUniversity(schoolName, schoolState, schoolLocation, schoolControl, numberStudents,
+					percentFemale, verbalSAT, mathSAT, schoolExpenses, percentFinancialAid, numApplicants,
+					percentAdmitted, percentEnrolled, academicScale, socialScale, qualityScale, emphases);
+		}
+	}
 
 
 	
